6859bf2a46f260eff8ededf63ecdb79dbdcabed8
Tobias Kask <tobias.kask@gmail.com>
2020-07-03 13:59:17 +0200

    Functional multi-user support via a user control table :O Also cleaned up and broke out the Whiteboard class to its own file.

--- src/app.lua
+++ src/app.lua
@@ -1,172 +1,17 @@
-
-local class = require('pl.class')
-local tablex = require('pl.tablex')
-local pretty = require('pl.pretty')
-local vec3 = require("modules.vec3")
-local mat4 = require("modules.mat4")
-local cairo = require("cairo")
+local Whiteboard = require("whiteboard")
 
 local client = Client(
     arg[1], 
     "allo-whiteboard"
 )
 local app = App(client)
-local BOARD_RESOLUTION = 128
 
 print("+==================+")
 print("+ WHITEBOARD ADDED +")
 print("+==================+")
 
-class.Whiteboard(ui.View)
 
-function Whiteboard:_init(bounds)
-  self:super(bounds)
-  print("+ initiating...    +")
-  print("+ ---------------- +")
-  print("+ width: " .. bounds.size.width .. "         +")
-  print("+ height: " .. bounds.size.height .. "        +")
-  print("+ ---------------- +")
-
-  self.isReadyForDrawing = false
-  self.drawingUser = nil
-  self.isDirty = false;
-  self.brushSize = 3;
-
-  self.sr = cairo.image_surface(cairo.cairo_format("rgb24"), bounds.size.width * BOARD_RESOLUTION, bounds.size.height * BOARD_RESOLUTION)  
-  self.cr = self.sr:context()
-
-  self:resetBoard()
-
-end
-
-function Whiteboard:specification()
-
-  self.sr:save_png("whiteboard.png")
-
-  local fh = io.open("whiteboard.png", "rb")
-  local image_to_convert = fh:read("*a")
-  fh:close()
-  local encoded_image = ui.util.base64_encode(image_to_convert)
-
-
-  local s = self.bounds.size
-  local w2 = s.width / 2.0
-  local h2 = s.height / 2.0
-  local d2 = s.depth / 2.0
-  local mySpec = tablex.union(ui.View.specification(self), {
-      geometry = {
-          type = "inline",
-          --          #tl?                #tr?              #bl?               #br?
-          vertices=   {{-w2, h2, 0.0},    {w2, h2, 0.0},    {-w2, -h2, 0.0},   {w2, -h2, 0.0}},
-          uvs=        {{0.0, 0.0},        {1.0, 0.0},       {0.0, 1.0},        {1.0, 1.0}},
-          triangles=  {{0, 1, 3},         {3, 2, 0},        {0, 2, 3},         {3, 1, 0}},
-          texture= encoded_image
-      },
-      collider= {
-          type= "box",
-          width= s.width, height= s.height, depth= s.depth
-      }
-  })
-  return mySpec
-end
-
-
-function Whiteboard:onInteraction(inter, body, sender)
-    if body[1] == "point" then
-        --print("pointing at the whiteboard with sender: ", sender);
-        local worldPoint = vec3(body[3][1], body[3][2], body[3][3])
-        local inverted = mat4.invert({}, self.bounds.pose.transform)
-        local localPoint = vec3(mat4.mul_vec4({}, inverted, {worldPoint.x, worldPoint.y, worldPoint.z, 1}))
-        
-        local localPointTopLeftOrigo = vec3(self.bounds.size.width/2 + localPoint.x, self.bounds.size.height/2 + localPoint.y, self.bounds.size.depth/2 + localPoint.z)
-        local normalizedLocalPointTopLeftOrigo = vec3(localPointTopLeftOrigo.x / self.bounds.size.width, localPointTopLeftOrigo.y / self.bounds.size.height, localPointTopLeftOrigo.z / self.bounds.size.depth)
-
-        -- print("----------------------")
-        -- print(worldPoint)
-        -- print(localPoint)
-        -- print(localPointTopLeftOrigo)
-        -- print(normalizedLocalPointTopLeftOrigo)
-
-        if (self.isReadyForDrawing and (sender == self.drawingUser)) then
-          --print("Drawing on the whiteboard...");
-          self:drawPixel( normalizedLocalPointTopLeftOrigo.x * self.bounds.size.width * BOARD_RESOLUTION, 
-                          normalizedLocalPointTopLeftOrigo.y * self.bounds.size.height * BOARD_RESOLUTION)
-        end
-
-    elseif body[1] == "point-exit" then
-        -- print("No longer pointing at the whiteboard");
-
-    elseif body[1] == "poke" then
-        -- set whiteboard to be "ready to recieve point events" when picking up "point" interactions
-        self.isReadyForDrawing = body[2]
-        self.drawingUser = sender
-    end
-end
-
-function Whiteboard:drawPixel(x, y)
-  --print("drawing @ x: ", x, " y: ", y)
-
-  self.cr:rgb(255, 255, 255)
-  self.cr:circle(x, y, self.brushSize)
-  self.cr:fill()
-  
-  self.isDirty = true
-end
-
-function Whiteboard:broadcastTextureChanged()
-  if self.app == nil then return end
-
-  local geom = self:specification().geometry
-  self:updateComponents({geometry = geom})
-  self.isDirty = false
-end
-
-function Whiteboard:sendIfDirty()
-  if self.isDirty then
-    self:broadcastTextureChanged()
-  end
-end
-
-function Whiteboard:setBrushSize(newbrushSize)
-  self.brushSize = newbrushSize
-end
-
-
-function Whiteboard:resetBoard()
-  print("Resetting board!")
-  
-  -- DRAWS THE WHOLE BOARD BLACK
-  self.cr:rgb(0, 0, 0)
-  self.cr:paint()
-
-  -- DRAWS ORIENTATION MARKERS
-  self.cr:rgb(255, 0, 0)    -- RED, TOP LEFT
-  self.cr:circle(0, 0, 16)
-  self.cr:fill()
-
-  self.cr:rgb(0, 255, 0)    -- GREEN, TOP RIGHT
-  self.cr:circle(self.bounds.size.width*BOARD_RESOLUTION, 0, 16)
-  self.cr:fill()
-
-  self.cr:rgb(255, 255, 0)  -- YELLOW, BOTTOM LEFT
-  self.cr:circle(0, self.bounds.size.height*BOARD_RESOLUTION, 16)
-  self.cr:fill()
-
-  self.cr:rgb(255, 0, 255)  -- MAGENTA, BOTTOM RIGHT
-  self.cr:circle(self.bounds.size.width*BOARD_RESOLUTION, self.bounds.size.height*BOARD_RESOLUTION, 16)
-  self.cr:fill()
-
-  -- DRAWS A BORDER ALONG THE EDGES OF THE WHITEBOARD
-  self.cr:rgb(255, 255, 255)  -- WHITE
-  self.cr:rectangle(0, 0, 5, self.bounds.size.height*BOARD_RESOLUTION)
-  self.cr:rectangle(self.bounds.size.width*BOARD_RESOLUTION-5, 0, 5, self.bounds.size.height*BOARD_RESOLUTION)
-  self.cr:rectangle(0, 0, self.bounds.size.width*BOARD_RESOLUTION, 5)
-  self.cr:rectangle(0, self.bounds.size.height*BOARD_RESOLUTION-5, self.bounds.size.width*BOARD_RESOLUTION, 5)
-  self.cr:fill()
-
-  self:broadcastTextureChanged()
-end
-
+-- CREATES THE WHITEBOARD
 local whiteboardView = Whiteboard(ui.Bounds(1.5, 1, 0,   2, 1, 0.1))
 
 -- ADDS THE GRAB HANDLE
@@ -199,7 +44,7 @@ whiteboardView:addSubview(brushSizeUpButton)
 
 app.mainView = whiteboardView
 
--- Runs sendIfDirty() 10 times per second
+-- Checks whiteboard refresh 10 times per second
 app:scheduleAction(0.1, true, function() 
   whiteboardView:sendIfDirty()
 end)
---
+++ src/whiteboard.lua
@@ -0,0 +1,167 @@
+local cairo = require("cairo")
+local class = require('pl.class')
+local tablex = require('pl.tablex')
+local vec3 = require("modules.vec3")
+local mat4 = require("modules.mat4")
+
+local BOARD_RESOLUTION = 128
+
+class.Whiteboard(ui.View)
+
+function Whiteboard:_init(bounds)
+  self:super(bounds)
+  print("+ initiating...    +")
+  print("+ ---------------- +")
+  print("+ width: " .. bounds.size.width .. "         +")
+  print("+ height: " .. bounds.size.height .. "        +")
+  print("+ ---------------- +")
+
+  self.isDirty = false;
+  self.brushSize = 3;
+
+  -- Table keeping tabs on users and if they initated intention (is allowed to draw)
+  -- {obj user, bool hasIntentToDraw}
+  self.drawingUserControlTable = {}
+
+  self.sr = cairo.image_surface(cairo.cairo_format("rgb24"), bounds.size.width * BOARD_RESOLUTION, bounds.size.height * BOARD_RESOLUTION)  
+  self.cr = self.sr:context()
+
+  self:resetBoard()
+
+end
+
+function Whiteboard:specification()
+
+  self.sr:save_png("whiteboard.png")
+
+  local fh = io.open("whiteboard.png", "rb")
+  local image_to_convert = fh:read("*a")
+  fh:close()
+  local encoded_image = ui.util.base64_encode(image_to_convert)
+
+
+  local s = self.bounds.size
+  local w2 = s.width / 2.0
+  local h2 = s.height / 2.0
+  local d2 = s.depth / 2.0
+  local mySpec = tablex.union(ui.View.specification(self), {
+      geometry = {
+          type = "inline",
+          --          #tl?                #tr?              #bl?               #br?
+          vertices=   {{-w2, h2, 0.0},    {w2, h2, 0.0},    {-w2, -h2, 0.0},   {w2, -h2, 0.0}},
+          uvs=        {{0.0, 0.0},        {1.0, 0.0},       {0.0, 1.0},        {1.0, 1.0}},
+          triangles=  {{0, 1, 3},         {3, 2, 0},        {0, 2, 3},         {3, 1, 0}},
+          texture= encoded_image
+      },
+      collider= {
+          type= "box",
+          width= s.width, height= s.height, depth= s.depth
+      }
+  })
+  return mySpec
+end
+
+
+function Whiteboard:onInteraction(inter, body, sender)
+    if body[1] == "point" then
+        self:_attemptToDraw(sender, body[3][1], body[3][2], body[3][3]);
+
+    elseif body[1] == "point-exit" then
+        -- print("No longer pointing at the whiteboard");
+
+    elseif body[1] == "poke" then
+        -- set whiteboard ability to pick up "point" interactions depending on poke is true or false.
+        self.drawingUserControlTable[sender] = body[2];
+
+        -- print("= USER CONTROL TABLE =")
+        -- for key,value in pairs(self.drawingUserControlTable) do 
+        --   print(key, value)
+        -- end
+    end
+
+end
+
+
+function Whiteboard:_attemptToDraw(sender, worldX, worldY, worldZ)
+  -- Checks if the user is allowed to draw
+  if (self.drawingUserControlTable[sender] ~= true ) then return end
+
+  local worldPoint = vec3(worldX, worldY, worldZ)
+  local inverted = mat4.invert({}, self.bounds.pose.transform)
+  local localPoint = vec3(mat4.mul_vec4({}, inverted, {worldPoint.x, worldPoint.y, worldPoint.z, 1}))
+  
+  local localPointTopLeftOrigo = vec3(self.bounds.size.width/2 + localPoint.x, self.bounds.size.height/2 + localPoint.y, self.bounds.size.depth/2 + localPoint.z)
+  local normalizedLocalPointTopLeftOrigo = vec3(localPointTopLeftOrigo.x / self.bounds.size.width, localPointTopLeftOrigo.y / self.bounds.size.height, localPointTopLeftOrigo.z / self.bounds.size.depth)
+
+  -- print("----------------------")
+  -- print(worldPoint)
+  -- print(localPoint)
+  -- print(localPointTopLeftOrigo)
+  -- print(normalizedLocalPointTopLeftOrigo)
+
+    self:_drawAt( normalizedLocalPointTopLeftOrigo.x * self.bounds.size.width * BOARD_RESOLUTION, 
+                    normalizedLocalPointTopLeftOrigo.y * self.bounds.size.height * BOARD_RESOLUTION)
+end
+
+function Whiteboard:_drawAt(x, y)
+  self.cr:rgb(255, 255, 255)
+  self.cr:circle(x, y, self.brushSize)
+  self.cr:fill()
+  
+  self.isDirty = true
+end
+
+function Whiteboard:broadcastTextureChanged()
+  if self.app == nil then return end
+
+  local geom = self:specification().geometry
+  self:updateComponents({geometry = geom})
+  self.isDirty = false
+end
+
+function Whiteboard:sendIfDirty()
+  if self.isDirty then
+    self:broadcastTextureChanged()
+  end
+end
+
+function Whiteboard:setBrushSize(newbrushSize)
+  self.brushSize = newbrushSize
+end
+
+
+function Whiteboard:resetBoard()
+  
+  -- DRAWS THE WHOLE BOARD BLACK
+  self.cr:rgb(0, 0, 0)
+  self.cr:paint()
+
+  -- DRAWS ORIENTATION MARKERS
+  self.cr:rgb(255, 0, 0)    -- RED, TOP LEFT
+  self.cr:circle(0, 0, 16)
+  self.cr:fill()
+
+  self.cr:rgb(0, 255, 0)    -- GREEN, TOP RIGHT
+  self.cr:circle(self.bounds.size.width*BOARD_RESOLUTION, 0, 16)
+  self.cr:fill()
+
+  self.cr:rgb(255, 255, 0)  -- YELLOW, BOTTOM LEFT
+  self.cr:circle(0, self.bounds.size.height*BOARD_RESOLUTION, 16)
+  self.cr:fill()
+
+  self.cr:rgb(255, 0, 255)  -- MAGENTA, BOTTOM RIGHT
+  self.cr:circle(self.bounds.size.width*BOARD_RESOLUTION, self.bounds.size.height*BOARD_RESOLUTION, 16)
+  self.cr:fill()
+
+  -- DRAWS A BORDER ALONG THE EDGES OF THE WHITEBOARD
+  self.cr:rgb(255, 255, 255)  -- WHITE
+  self.cr:rectangle(0, 0, 5, self.bounds.size.height*BOARD_RESOLUTION)
+  self.cr:rectangle(self.bounds.size.width*BOARD_RESOLUTION-5, 0, 5, self.bounds.size.height*BOARD_RESOLUTION)
+  self.cr:rectangle(0, 0, self.bounds.size.width*BOARD_RESOLUTION, 5)
+  self.cr:rectangle(0, self.bounds.size.height*BOARD_RESOLUTION-5, self.bounds.size.width*BOARD_RESOLUTION, 5)
+  self.cr:fill()
+
+  self:broadcastTextureChanged()
+end
+
+return Whiteboard
