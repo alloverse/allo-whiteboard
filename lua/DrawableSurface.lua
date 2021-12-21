78740875883aabb7cea38fd6b3004d47c3a91f15
Patrik Sjöberg <voxxar@gmail.com>
2021-12-21 12:44:00 +0100

    Recreate the video surface on resize

--- lua/DrawableSurface.lua
+++ lua/DrawableSurface.lua
@@ -9,7 +9,7 @@ local ffi = require('ffi')
 
 local BOARD_RESOLUTION = 512
 
-class.DrawableSurface(ui.VideoSurface)
+class.DrawableSurface(ui.View)
 
 function DrawableSurface:_init(bounds)
   self:super(bounds)
@@ -18,6 +18,8 @@ function DrawableSurface:_init(bounds)
 
   -- Table keeping tabs on users and if they initated intention (is allowed to draw)
   self.accessControlTable = {}
+
+  self.videoSurface = nil
 
   local width = bounds.size.width * BOARD_RESOLUTION
   local height = bounds.size.height * BOARD_RESOLUTION
@@ -28,6 +30,7 @@ function DrawableSurface:_init(bounds)
 
   self.backgroundColor = {0.051,0.023,0.101}
   self.brushColor = {1, 1, 1}
+
 
 
   self:clearBoard()
@@ -119,12 +122,10 @@ function DrawableSurface:_drawAt(sender, x, y)
 end
 
 function DrawableSurface:broadcastTextureChanged()
-  if self.trackId then
-    self.sr:flush()
-    local bitmap = self.sr:bitmap()
-    local bpp = bitmap.stride / bitmap.w
-    self:sendFrame(ffi.string(bitmap.data, (bitmap.stride/bpp) * bitmap.h * bpp), bitmap.w, bitmap.h, bitmap.format, bitmap.stride)
-  end
+  self.sr:flush()
+  local bitmap = self.sr:bitmap()
+  local bpp = bitmap.stride / bitmap.w
+  self.videoSurface:sendFrame(ffi.string(bitmap.data, (bitmap.stride/bpp) * bitmap.h * bpp), bitmap.w, bitmap.h, bitmap.format, bitmap.stride)
 end
 
 function DrawableSurface:clearBoard()
@@ -147,15 +148,15 @@ function DrawableSurface:resize(newWidth, newHeight, commit)
 
   if (oldWidth == newWidth and oldHeight == newHeight) then return false end
 
-  local newCalculatedWidth = newWidth * BOARD_RESOLUTION
-  local newCalculatedHeight = newHeight * BOARD_RESOLUTION
+  local newCalculatedWidth = math.floor(newWidth * BOARD_RESOLUTION + 0.5)
+  local newCalculatedHeight = math.floor(newHeight * BOARD_RESOLUTION + 0.5)
   local oldCalculatedWidth = oldWidth * BOARD_RESOLUTION
   local oldCalculatedHeight = oldHeight * BOARD_RESOLUTION
 
   if commit then
     print("committing to new size")
-    local newSourceX = math.floor(((newCalculatedWidth - oldCalculatedWidth)/2)+0.5)
-    local newSourceY = math.floor(((newCalculatedHeight - oldCalculatedHeight)/2)+0.5)
+    local newSourceX = ((newCalculatedWidth - oldCalculatedWidth)/2)
+    local newSourceY = ((newCalculatedHeight - oldCalculatedHeight)/2)
     
     local newsr = cairo.image_surface(cairo.cairo_format("rgb24"), newCalculatedWidth, newCalculatedHeight)  
     local newcr = newsr:context()
@@ -176,11 +177,24 @@ function DrawableSurface:resize(newWidth, newHeight, commit)
   self.bounds.size.width = newWidth
   self.bounds.size.height = newHeight 
 
+  self.videoSurface.bounds.size.width = newWidth
+  self.videoSurface.bounds.size.height = newHeight
+  self.videoSurface:setBounds()
+
   self:updateComponents(
     self:specification()
   )
 
   return true
+end
+
+function DrawableSurface:setResolution(width, height)
+  if self.videoSurface then 
+    self.videoSurface:removeFromSuperview()
+  end
+
+  self.videoSurface = VideoSurface(ui.Bounds(0, 0, 0.003, self.bounds.size.width, self.bounds.size.height, 0), {width, height})
+  self:addSubview(self.videoSurface)
 end
 
 
